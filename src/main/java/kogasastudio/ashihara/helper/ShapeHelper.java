0a0861a82b033949d4f71ffc9f9b35a107060630
Kogasa-Studio <nuclearflashie@outlook.com>
2022-04-10 15:57:18 +0800

    添加一些贴图

--- src/main/java/kogasastudio/ashihara/helper/ShapeHelper.java
+++ src/main/java/kogasastudio/ashihara/helper/ShapeHelper.java
@@ -1,19 +1,33 @@
 package kogasastudio.ashihara.helper;
 
-//import net.minecraft.block.Block;
-//import net.minecraft.util.Direction;
-//import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.block.Block;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.shapes.VoxelShape;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ShapeHelper
 {
-//    public static VoxelShape rotateShapeBy90(VoxelShape shape)
-//    {
-//        double xS = shape.getStart(Direction.Axis.X);
-//        double xE = shape.getEnd(Direction.Axis.X);
-//        double yS = shape.getStart(Direction.Axis.Y);
-//        double yE = shape.getEnd(Direction.Axis.Y);
-//        double zS = shape.getStart(Direction.Axis.Z);
-//        double zE = shape.getEnd(Direction.Axis.Z);
-//        return Block.makeCuboidShape(zS, yS, xS, zE, yE, xE);
-//    }
+    public static Map<Direction, VoxelShape> genFourWayShapes(Direction dir, double xS, double yS, double zS, double xE, double yE, double zE)
+    {
+        Map<Direction, VoxelShape> shapes = new HashMap<>();
+
+        shapes.put(dir, rotateBy90(dir, xS, yS, zS, xE, yE, zE));
+        shapes.put(dir.rotateY(), rotateBy90(dir.rotateY(), xS, yS, zS, xE, yE, zE));
+        shapes.put(dir.getOpposite(), rotateBy90(dir.getOpposite(), xS, yS, zS, xE, yE, zE));
+        shapes.put(dir.getOpposite().rotateY(), rotateBy90(dir.getOpposite().rotateY(), xS, yS, zS, xE, yE, zE));
+
+        return shapes;
+    }
+
+    private static VoxelShape rotateBy90(Direction dir, double xS, double yS, double zS, double xE, double yE, double zE)
+    {
+        switch (dir.getAxis())
+        {
+            case Z: return Block.makeCuboidShape(16d - zE, yS, xS, 16d - zS, yE, xE);
+            case X: return Block.makeCuboidShape(zS, yS, xS, zE, yE, xE);
+            default:return Block.makeCuboidShape(xS, yS, zS, xE, yE, zE);
+        }
+    }
 }
