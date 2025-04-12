9fdcc631930a16b46153fa1da95851872fd0d1c5
Kogasa <36784108+Kogasa-Studio@users.noreply.github.com>
2025-04-12 22:00:24 +0800

    #Modification: Complete the GuideBook texture, slightly fix the animation.

--- src/main/java/kogasastudio/ashihara/client/gui/GuideBookScreen.java
+++ src/main/java/kogasastudio/ashihara/client/gui/GuideBookScreen.java
@@ -29,6 +29,9 @@ import java.util.Map;
 
 import static kogasastudio.ashihara.utils.OptionalUtil.getWithDefault;
 
+/*
+ * Thanks to @TT432 for helping to debug animation.
+ */
 public class GuideBookScreen extends Screen
 {
     private final GuideBookModel book = new GuideBookModel();
@@ -37,7 +40,7 @@ public class GuideBookScreen extends Screen
     private int ticks = 0;
     private int coolDown = 0;
 
-    private int currentPageIndex = 145;
+    private int currentPageIndex = 144;
     private Map<Integer, Pair<MutableFloat, String>> flipQueue = new HashMap<>();
 
     public GuideBookScreen(Component title, Player player)
